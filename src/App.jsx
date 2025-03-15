befe043966636262d61741e813b4370143ddb8de
carthy27 <03thomasmc@gmail.com>
2025-03-15 19:14:41 +0000

    Deployment fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import AuthGuard from "./AuthGaurd";
 
 export default function App() {
   return (
-    <Router basename="/CS4227-Architecture">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/profile"
-          element={
-            <AuthGuard>
-              <Profile />
-            </AuthGuard>
-          }
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route
+        path="/profile"
+        element={
+          <AuthGuard>
+            <Profile />
+          </AuthGuard>
+        }
+      />
+    </Routes>
   );
 }
