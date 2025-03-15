91ee63c09227ae36c49af144e1ee9fc6963b042f
carthy27 <03thomasmc@gmail.com>
2025-03-15 20:34:04 +0000

    THis time

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Router,Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import AuthGuard from "./AuthGaurd";
