4baa5812b14265637a026e56a7f363c4bf989313
carthy27 <03thomasmc@gmail.com>
2025-03-15 20:28:30 +0000

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
-import { Routes, Route } from "react-router-dom";
+import { Router,Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import AuthGuard from "./AuthGaurd";
 
 export default function App() {
   return (
-    <Routes>
-      <Route index element={<Login />} />
-      <Route path="login" element={<Login />} />
-      <Route
-        path="profile"
-        element={
-          <AuthGuard>
-            <Profile />
-          </AuthGuard>
-        }
-      />
-    </Routes>
+    <Router>
+          <Routes>
+                <Route path="/" element={<Login />} />
+                <Route  path="/profile"  element={
+                    <AuthGuard>
+                      <Profile />
+                    </AuthGuard>
+                  }
+                />
+          </Routes>
+    </Router>
+  );
+}
+
+
+function App() {
+  return (
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/News" element={<News />} />
+
+
+              <Route path="/Workspace" element={<Workspace />} />
+              <Route path="/createWorkspace" element={<CreateWorkspace />} />
+              <Route path="/viewWorkspace" element={<ViewWorkspace />} />
+
+
+              <Route path="/workspaceChat/:id" element={<WorkspaceChat />} />
+
+          </Routes>
+      </Router>
   );
 }
